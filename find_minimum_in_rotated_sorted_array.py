19409ce31a5bcbf83e82ae0142c7a859ee408480
Anushri <sastikaranushri@gmail.com>
2025-11-17 16:15:53 -0500

    implementation with an explaination

--- find_minimum_in_rotated_sorted_array.py
+++ find_minimum_in_rotated_sorted_array.py
@@ -1,25 +1,26 @@
 # https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
 
-# Using the binary search finding the minimum number. In the first case checked the mid is minimum than previous and next element if this condition satisfies then return it. Else check the array is left or righ sorted and move the pointer accordingly. Time Complexity O(log n)
+# Using the binary search finding the minimum number. In the first condition check the low is less than high if yes we go the minimum number. In 'and' case check the mid is minimum than previous and next element if this condition satisfies then return it. Else check the array is left or righ sorted and move the pointer accordingly. Time Complexity O(log n)
 
 class Solution:
     def findMin(self, nums : list[int]) -> int:
         low = 0
         high = len(nums) - 1
         while low <= high:
-            # very first check if the left is smaller than the right. it means array is sorted and we got the minimum number
+            # first check is array sorted, if yes, we got the minimum number
             if nums[low] <= nums[high]:
                 return nums[low]
+            # find the mid point
             mid = (low + high) // 2 
-            # this  "or" condition is the edge case along with that check the mid is smaller than the previous and smaller than the nex
+            # this edge case check the mid is smaller than the previous and smaller than the next, if yes, we got the minimum number
             if (mid == 0 or nums[mid] < nums[mid - 1]) and (mid == high - 1 or nums[mid]< nums[mid+1]):
                 return nums[mid]
             # this condition means left half is sorted
             elif nums[low] <= nums[mid]:
-                # so as the element is not in the left side move the pointer to right. why element is not in the left side because we already check the low in very first condition after while
+                # the element is not in the left side move the pointer to right.
                 low = mid + 1
-            # when the left is not sorted means right is sorted so move the pointer to the left part
-            else:
+            # when the left is not sorted so move the pointer to the left part
+            else:   
                 high = mid - 1
         return nums[low]
     
